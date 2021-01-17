4be533ca35f18fdcd69c78b8a083b87f135604bc
Harold <harold.goldensky@gmail.com>
2021-01-17 20:13:16 +0800

    Add files via upload

---
+++ assets/js/swiper.js
@@ -0,0 +1,31 @@
+// ===== JS SWIPER ==== //
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    slidesPerView: 1,
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    //AutoPlay
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    
+    //Min-width
+    breakpoints: {
+        900: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+        604:{
+            slidesPerView: 2,
+            spaceBetween: 20,
+        }  
+    }
+});
