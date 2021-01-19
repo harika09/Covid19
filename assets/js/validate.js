7a7d07e5faf6c58d0a4743f57d331eeaad2ed81f
Harold <harold.goldensky@gmail.com>
2021-01-19 18:31:28 +0800

    Update validate.js

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -1,5 +1,27 @@
+$("#btn-send").click(function(){
+
+    const contactName = document.getElementById('contact-name').value;
+    const contactEmail = document.getElementById('contact-email').value;
+    const contactMessage = document.getElementById('contact-message').value;
+    const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if(contactName === '' || contactEmail === '' || contactMessage === ''){
+        alert("Fields cannot be empty");
+        
+    } else if(contactEmail.match(pattern)){
+        validEmail();
+    }
+
+    else{
+        invalidEmail();
+    }
 
 
+});
+
+
+
+//Subscribe Validation
 $("#subscribe").click(function(){
 
     const email = document.getElementById('email').value;
@@ -19,6 +41,8 @@ $("#subscribe").click(function(){
 
 
 });
+
+
 
 function emptyEmail(){
     Swal.fire({
