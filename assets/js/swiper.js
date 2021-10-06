39a8fe44599a7c5cb95bd730024f15063855e994
harika09 <harold.goldensky.com>
2021-10-06 20:32:31 +0800

    fix swiper slider

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,31 +1,31 @@
 // ===== JS SWIPER ==== //
-var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    slidesPerView: 1,
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  slidesPerView: 1,
+  direction: "horizontal",
+  loop: true,
 
-    //AutoPlay
-    autoplay: {
-        delay: 3500,
-        disableOnInteraction: false,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  //AutoPlay
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+
+  //Min-width
+  breakpoints: {
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 20,
     },
-    
-    //Min-width
-    breakpoints: {
-        900: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-          },
-        604:{
-            slidesPerView: 2,
-            spaceBetween: 20,
-        }  
-    }
+    604: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
 });
